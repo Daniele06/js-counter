668cde27a248ea11f6415d80a9f2ca037bdc6144
Daniele <47061226+Daniele06@users.noreply.github.com>
2021-03-19 15:41:20 +0100

    Add files via upload

---
+++ 5. Counter/script.js
@@ -0,0 +1,26 @@
+var number = 0;
+
+// decrease number and turn count red
+document.getElementById("decrease").onclick = function() {
+    number--;
+    document.getElementById("count").innerHTML = number;
+
+    if(number < 0) {
+        document.getElementById("count").style.color = "red";
+    }
+}
+
+// Increase number and turn count green
+document.getElementById("increase").onclick = function() {
+    number++;
+    document.getElementById("count").innerHTML = number;
+
+    if(number >= 0) {
+        document.getElementById("count").style.color = "#26c026";
+    }
+}
+
+// Save number
+document.getElementById("save").onclick = function() {
+    document.getElementById("saved").innerHTML = number;
+}
